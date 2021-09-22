e5c7a1728b67b8b2b900ad71589d7b54ad389044
beck <beckyenwen@gmail.com>
2021-09-22 16:04:01 -0600

    modify

--- pages/attractions/add.js
+++ pages/attractions/add.js
@@ -3,7 +3,7 @@ import Layout from '../../components/Layout'
 export default function AddAttractions() {
     return (
         <Layout title='Add New Attraction'>
-            <h1>Add Attraction</h1>
+            <h1>Add Attraction!</h1>
         </Layout>
     )
 }
