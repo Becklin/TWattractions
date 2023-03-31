0dff929132e23adc7ad7e6da496729c61f26cf27
beck <beckyenwen@gmail.com>
2023-03-31 13:49:03 -0600

    modify search api

--- pages/attractions/search.js
+++ pages/attractions/search.js
@@ -1,4 +1,5 @@
 import Layout from "@/components/Layout";
+import ListLayout from "@/components/ListLayout";
 import AttractionItem from "@/components/AttractionItem";
 import qs from "qs";
 import { useRouter } from "next/router";
@@ -8,40 +9,46 @@ import { API_URL } from "@/config/index";
 export default function SearchPage({ attractions }) {
   const router = useRouter();
   return (
-    <Layout title="Search Results">
+    <>
       <Link href="/">Back</Link>
       <h1>Search Results for {router.query.keyword}</h1>
       {attractions.length === 0 && <h3>No Attractions to show</h3>}
       {attractions.map((attraction) => {
         return <AttractionItem key={attraction.id} attraction={attraction} />;
       })}
-    </Layout>
+    </>
   );
 }
 
-export async function getServerSideProps({ query: { keyword } }) {
-  const query = qs.stringify({
-    _where: {
-      _or: [
-        { name_contains: keyword },
-        { location_contains: keyword },
-        // { introduction_contains: keyword },
-      ],
-    },
-  });
+SearchPage.getLayout = function getLayout(page) {
+  return (
+    <Layout title="Search Results">
+      <ListLayout>{page}</ListLayout>
+    </Layout>
+  );
+};
 
+export async function getServerSideProps({ query: { keyword } }) {
+  const query = qs.stringify(
+    {
+      sort: ["name:asc"],
+      filters: {
+        name: {
+          $eq: keyword,
+        },
+      },
+      populate: "*",
+      fields: ["name"],
+      publicationState: "live",
+      locale: ["en"],
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
   const res = await fetch(`${API_URL}/attractions?${query}`);
   const response = await res.json();
   return {
     props: { attractions: response.data },
   };
 }
-
-// export async function getServerSideProps() {
-//   const res = await fetch(`${API_URL}/attractions`);
-//   const attractions = await res.json();
-//   console.log('attractions', attractions);
-//   return {
-//     props: { attractions },
-//   }
-// }
