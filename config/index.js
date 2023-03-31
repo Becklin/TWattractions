dec1647b6f7a81e098d610bf74ea5405a6cf33d6
beck <beckyenwen@gmail.com>
2023-03-30 21:59:34 -0600

    change config url

--- config/index.js
+++ config/index.js
@@ -1,8 +1,6 @@
 export const API_URL =
-  //process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337/api/";
-  "http://127.0.0.1:1337/api";
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337/api/";
 
-// export const NEXT_URL =
-//   process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
-export const NEXT_URL = "http://localhost:3000";
+export const NEXT_URL =
+  process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
 export const PER_PAGE = 5;
