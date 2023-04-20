35fcfbbcf7e973d438ceca9f52425577d9c0b063
beck <beckyenwen@gmail.com>
2023-04-20 14:42:17 -0600

    add analyze lib

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-module.exports = {
+// module.exports = {
+//   reactStrictMode: false,
+//   images: {
+//     domains: ["res.cloudinary.com", "www.travel.taipei"],
+//   },
+//   i18n: {
+//     locales: ["en"],
+//     defaultLocale: "en",
+//   },
+// };
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  env: {
+    NEXT_PUBLIC_ENV: "PRODUCTION", //your next configs goes here
+  },
   reactStrictMode: false,
   images: {
     domains: ["res.cloudinary.com", "www.travel.taipei"],
@@ -7,4 +25,4 @@ module.exports = {
     locales: ["en"],
     defaultLocale: "en",
   },
-};
+});
